0939e25751ab9ad69ac0a54d64b6b16a86e1ad8d
Ausaf Muhammed K.A <ausafmuhammedkaofficial@gmail.com>
2025-11-15 12:29:48 +0530

    Time: 1 ms (45.71%) | Memory: 55.6 MB (42.38%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var findGCD = function(nums) {
-     let num = nums.sort((a,b)=>a-b)
-    let num1 = num[0]
-    let num2 = num[num.length-1]
+     let num = nums.sort((a,b)=>a-b);
     let gcd = (x,y)=> y === 0 ? x : gcd(y,x%y);
-    let g = gcd(num1,num2)
+    let g = gcd(num[0],num[num.length-1]);
     return g;
 };
