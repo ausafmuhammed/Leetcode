c75979f2d10f266241bf285b721f4c0f04c57005
Ausaf Muhammed K.A <ausafmuhammedkaofficial@gmail.com>
2026-02-27 10:19:20 +0530

    Time: 3 ms (35.32%) | Memory: 19.3 MB (59.69%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2:
+            return False
+
+        pairs = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for char in s:
+            if char in pairs:
+                if not stack or stack.pop() != pairs[char]:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
